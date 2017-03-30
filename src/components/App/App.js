c95f71996b1d7c0bde102fd7ccbd3b9e9c4c66c0
arshdkhn1 <arshdkhn1@gmail.com>
2017-03-30 22:45:52 +0530

    adding update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World!
+        <button className="dialogBtn">Event</button>
+        <button className="dialogBtn">Task</button>
+        <button className="dialogBtn">Edit</button>
       </div>
     );
   }
